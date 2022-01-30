6d0ae490086dda91cb04313f61f3da26d8a35091
AllrightIsHere <gasilva912@gmail.com>
2022-01-30 15:52:23 -0300

    Exercício 01: add pessoa

--- routes/people.js
+++ routes/people.js
@@ -1,22 +1,19 @@
-import express from 'express'
-import db from'../db.js'
-const router = express.Router()
-
+import express from 'express';
+import db from '../db.js';
+const router = express.Router();
 
 /* GET lista de pessoas. */
 router.get('/', async (req, res, next) => {
-
   try {
     const [people] = await db.execute({
       sql: 'SELECT * FROM person LEFT OUTER JOIN zombie ON eatenBy = zombie.id',
-  
+
       // nestTables resolve conflitos de haver campos com mesmo nome nas tabelas
       // nas quais fizemos JOIN (neste caso, `person` e `zombie`).
       // descrição: https://github.com/felixge/node-mysql#joins-with-overlapping-column-names
-      nestTables: true
-    })
+      nestTables: true,
+    });
 
-    
     // Exercício 3: negociação de conteúdo para esta resposta
     //
     // renderiza a view de listagem de pessoas, passando como contexto
@@ -29,57 +26,55 @@ router.get('/', async (req, res, next) => {
     res.render('list-people', {
       people,
       success: req.flash('success'),
-      error: req.flash('error')
-    })
-
+      error: req.flash('error'),
+    });
   } catch (error) {
-    console.error(error)
-    error.friendlyMessage = 'Problema ao recuperar pessoas'
-    next(error)
+    console.error(error);
+    error.friendlyMessage = 'Problema ao recuperar pessoas';
+    next(error);
   }
-})
-
+});
 
 /* PUT altera pessoa para morta por um certo zumbi */
 router.put('/eaten/', async (req, res, next) => {
-  const zombieId = req.body.zombie
-  const personId = req.body.person
+  const zombieId = req.body.zombie;
+  const personId = req.body.person;
 
   if (!zombieId || !personId) {
-    req.flash('error', 'Nenhum id de pessoa ou zumbi foi passado!')
-    res.redirect('/')
+    req.flash('error', 'Nenhum id de pessoa ou zumbi foi passado!');
+    res.redirect('/');
     return;
   }
 
   try {
-    const [result] = await db.execute(`UPDATE person 
+    const [result] = await db.execute(
+      `UPDATE person 
                                        SET alive=false, eatenBy=?
                                        WHERE id=?`,
-                                      [zombieId, personId])
+      [zombieId, personId]
+    );
     if (result.affectedRows !== 1) {
-      req.flash('error', 'Não há pessoa para ser comida.')
+      req.flash('error', 'Não há pessoa para ser comida.');
     } else {
-      req.flash('success', 'A pessoa foi inteiramente (não apenas cérebro) engolida.')
+      req.flash(
+        'success',
+        'A pessoa foi inteiramente (não apenas cérebro) engolida.'
+      );
     }
-    
   } catch (error) {
-    req.flash('error', `Erro desconhecido. Descrição: ${error}`)
-
+    req.flash('error', `Erro desconhecido. Descrição: ${error}`);
   } finally {
-    res.redirect('/')
+    res.redirect('/');
   }
-
-})
-
+});
 
 /* GET formulario de registro de nova pessoa */
 router.get('/new/', (req, res) => {
   res.render('new-person', {
     success: req.flash('success'),
-    error: req.flash('error')
-  })
-})
-
+    error: req.flash('error'),
+  });
+});
 
 /* POST registra uma nova pessoa */
 // Exercício 1: IMPLEMENTAR AQUI
@@ -88,7 +83,42 @@ router.get('/new/', (req, res) => {
 //   2. Redirecionar para a rota de listagem de pessoas
 //      - Em caso de sucesso do INSERT, colocar uma mensagem feliz
 //      - Em caso de erro do INSERT, colocar mensagem vermelhinha
+router.post('/', async (req, res) => {
+  const { name } = req.body;
 
+  if (!name) {
+    req.flash('error', 'Digite o nome da nova pessoa.');
+    res.redirect('/people');
+    return;
+  }
+
+  const transaction = await db.getConnection();
+
+  try {
+    await transaction.beginTransaction();
+
+    await transaction.execute(
+      `INSERT INTO person (name, alive)
+      VALUES (?, ?)`,
+      [name, 1]
+    );
+
+    await transaction.commit();
+    req.flash('success', 'Pessoa criada com sucesso!');
+  } catch (e) {
+    try {
+      if (transaction) {
+        await transaction.rollback();
+      }
+    } catch (err) {}
+
+    console.error(e);
+    e.friendlyMessage = 'Erro: Não foi possível criar a nova pessoa.';
+  } finally {
+    await transaction.release();
+    res.redirect('/people');
+  }
+});
 
 /* DELETE uma pessoa */
 // Exercício 2: IMPLEMENTAR AQUI
@@ -98,5 +128,4 @@ router.get('/new/', (req, res) => {
 //      - Em caso de sucesso do INSERT, colocar uma mensagem feliz
 //      - Em caso de erro do INSERT, colocar mensagem vermelhinha
 
-
-export default router
+export default router;
